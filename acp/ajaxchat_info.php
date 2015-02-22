420a9f1db68fcf6d9fd5ba078627e87a86bc3193
LMOspaceace <spaceace@livemembersonly.com>
2015-02-22 12:48:19 -0500

    Create ajaxchat_info.php

---
+++ acp/ajaxchat_info.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *
+ * Ajax Chat extension for phpBB.
+ *
+ * @copyright (c) 2015 spaceace <http://www.livemembersonly.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace spaceace\ajaxchat\acp;
+
+class ajaxchat_info
+{
+
+    function module()
+    {
+        return array(
+            'filename' => '\spaceace\ajaxchat\acp\ajaxchat_module',
+            'title'    => 'ADMIN_AJAXCHAT',
+            'modes'    => array(
+                'settings' => array(
+                    'title' => 'ADMIN_AJAXCHAT_SETTINGS',
+                    'auth'  => 'ext_spaceace/ajaxchat && acl_a_board',
+                    'cat'   => array('ADMIN_AJAXCHAT')
+                ),
+            ),
+        );
+    }
+
+}
