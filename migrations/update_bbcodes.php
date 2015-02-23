48467e19b5beb2fe064dc022253e637c35558c1e
Darin Wilton <spaceace@livemembersonly.com>
2015-02-23 17:18:49 -0500

    Update update_bbcodes.php
    edited for coding guidelines

--- migrations/update_bbcodes.php
+++ migrations/update_bbcodes.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- *
- * Ajax Chat extension for phpBB.
- *
- * @copyright (c) 2015 spaceace <http://www.livemembersonly.com>
- * @license GNU General Public License, version 2 (GPL-2.0)
- *
- */
+*
+* Ajax Chat extension for phpBB.
+*
+* @copyright (c) 2015 spaceace <http://www.livemembersonly.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
 
 namespace spaceace\ajaxchat\migrations;
 
@@ -36,5 +36,4 @@ class update_bbcodes extends \phpbb\db\migration\migration
         $acp_manager = new \spaceace\ajaxchat\core\acp_manager($this->db, $request, $user, $this->phpbb_root_path, $this->php_ext);
         $acp_manager->install_bbcodes($bbcode_data);
     }
-
 }
