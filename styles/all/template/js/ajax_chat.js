5326513c8bf4709b7260877edd3502a5de513a44
Darin Wilton <spaceace@livemembersonly.com>
2015-09-01 08:15:15 -0400

    removed onload function

--- styles/all/template/js/ajax_chat.js
+++ styles/all/template/js/ajax_chat.js
@@ -259,10 +259,3 @@ function addText(instext)
 	}
 }
 //END;Whatever
-window.onload = function () {
-	for (var i = 0, l = document.getElementsByTagName('input').length; i < l; i++) {
-		if (document.getElementsByTagName('input').item(i).type == 'text') {
-			document.getElementsByTagName('input').item(i).setAttribute('autocomplete', 'off');
-		}
-	}
-};
