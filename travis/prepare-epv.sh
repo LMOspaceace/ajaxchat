0c4a7b39af58373cf7098bbbcc3da12de219b800
Darin Wilton <spaceace@livemembersonly.com>
2017-08-24 18:20:41 -0400

    Update prepare-epv.sh

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -5,19 +5,15 @@
 # @copyright (c) phpBB Limited <https://www.phpbb.com>
 # @license GNU General Public License, version 2 (GPL-2.0)
 #
-# For full copyright and license information, please see
-# the docs/CREDITS.txt file.
-#
 set -e
 set -x
 
-EPV=$1
-NOTESTS=$2
+DB=$1
+TRAVIS_PHP_VERSION=$2
 
-if [ "$EPV" == "1" -a "$NOTESTS" == "1" ]
+if [ "$DB" == "mysqli" -a "$TRAVIS_PHP_VERSION" == "5.5" ]
 then
 	cd phpBB
-	composer remove sami/sami --update-with-dependencies --dev --no-interaction
-	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
+	composer require phpbb/epv:dev-master --dev --no-interaction
 	cd ../
 fi
